01ad80054d1c74461e0008c86029a858ecd714a4
Collin Ahn <collinahn@hufs.ac.kr>
2021-08-01 03:54:29 +0900

    [Add] common constants reference file

---
+++ constantsLT.py
@@ -0,0 +1,15 @@
+#-*- encoding: utf-8 -*-
+
+# 상수를 한 곳에서 관리하도록 하는 파이썬 파일
+# 값은 변경하지 않는다. 
+# 변수명은 반드시 대문자와 언더바로 구성한다.
+# 사용예)
+'''
+import constantsLT as const
+
+print(const.EXAMPLE_CONST)
+'''
+
+# 2021.08.01 created by taeyoung
+
+STOCK_VALUE_QUEUE_SIZE = 100
