448bfaf25104e363f3bb93d0efb5cb0ba82dcfbd
jiuk96 <jiuk96@naver.com>
2021-08-12 23:25:32 +0900

    [Modify] 로거추가, import 수정완료, 매수/매도 함수 update (미완)

--- KiwoomMain.py
+++ KiwoomMain.py
@@ -6,6 +6,7 @@
 ## wriiten by: Jiuk Jang 
 # 2021-08-08 로그인 및 로그인 관련 정보, 주식기본정보요청, 체결정보요청(미완) 관련 함수 만듦 
 # 2021-08-09 체결정보요청 완성, 매수/매도 함수(미완)
+# 2021-08-12 로거 추가, import 수정 완료, 매수/매도 함수 update (미완)
 
 ## written by: ChanHyuk Jun
 
@@ -13,14 +14,16 @@
 ## 설명: KiwoomAPI 파일에서 api관련 함수들을 다 다루고, KiwoomMain에서 실제 거래와 관련된 함수들을 만들어 다룬다.
 
 import sys
+# from LoggerLT import Logger
 from KiwoomAPI import KiwoomAPI
-from PyQt5.QtWidgets import *
-from TR_Code import *
+from PyQt5.QtWidgets import QApplication
+from TR_Code import output_list
 
 class KiwoomMain:
     def __init__(self):
         self.kiwoom = KiwoomAPI()
         self.kiwoom.login()
+        # self.log = Logger()
 # ----------- #
     ##사용가능한 함수들
     # 내 로그인 정보 불러오기 (로그인 상태, 이름, ID, 계좌 개수, 계좌번호) 8/8일 작성
@@ -58,19 +61,33 @@ class KiwoomMain:
         istr_volume_power = self.kiwoom.rq_data['OPT10003']['Data'][0]['체결강도']
 
         return istr_current_price, istr_volume_power
-
+    
+    # 시장가 매수 (확인 x) 8/12 수정
     def Stock_Buy_Marketprice(self, istr_stock_code, istr_account_number, in_quantity):
         self.kiwoom.SendOrder("시장가매수", "0101", istr_account_number, 1, istr_stock_code, in_quantity, 0, "03", "")
+        # self.log.INFO("시장가매수: " + self.kiwoom.SendOrder("시장가매수", "0101", istr_account_number, 1, istr_stock_code, in_quantity, 0, "03", ""))
+        return self.kiwoom.mlist_chejan_data
 
-    # def stock_
+    # 시장가 매도 8/12 시작
+    def Stock_Sell_Marketprice(self, istr_stock_code, istr_account_number, in_quantity):
+        self.kiwoom.SendOrder("시장가매수", "0101", istr_account_number, 1, istr_stock_code, in_quantity, 0, "03", "")
+    
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     api_con = KiwoomMain()
     # 아래는 테스트를 위한 것이니 신경 쓰지 않아도 됨
-    a= api_con.OPT10001('005930')
+
+    # account = api_con.Get_Login_Info()
+    # log.INFO(api_con.Stock_Buy_Marketprice('035720', account[4], 10))
+    # a= api_con.OPT10001('005930')
     # print(a)
-    result5, result7= api_con.OPT10003('035720')
+    # result5, result7= api_con.OPT10003('035720')
     # print(result5, result7)
     account = api_con.Get_Login_Info()
     # print(type(account[4]))
-    api_con.Stock_Buy_Marketprice('035720', account[4], 10)
+
+    r = api_con.Stock_Buy_Marketprice('035720', account[4], 10)
+    print(r)
+    s = api_con.Stock_Buy_Marketprice('005930', account[4], 10)
+    print(s)
