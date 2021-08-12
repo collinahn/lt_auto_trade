f5004d8ed4ed1466e2ce11d7565e92436d9de00e
Collin Ahn <collinahn@hufs.ac.kr>
2021-08-12 21:32:54 +0900

    [Add] sharedMem 업데이트하는 첫 번째 스레드

--- RunThread.py
+++ RunThread.py
@@ -11,13 +11,17 @@
 # 2021.08.02 created by 태영
 
 
-from KiwoomAPI import KiwoomAPI
 import sys
+import time
+import threading
+import constantsLT as const
+from datetime import datetime
 from PyQt5.QtWidgets import QApplication
+from KiwoomAPI import KiwoomAPI
 from KiwoomMain import KiwoomMain
 from SharedMem import SharedMem
+from GetPutDB import GetPutDB
 from LoggerLT import Logger
-import threading
 
 class RunThread(object):
 
@@ -33,12 +37,37 @@ class RunThread(object):
             # if문 내부에서 초기화 진행
 
             #키움 초기화 및 로그인 처리
-            qapp = QApplication(sys.argv)
-            kapi = KiwoomMain()
+            self.qapp = QApplication(sys.argv)
+            self.kapi = KiwoomMain()
             
             #유저 정보를 받아와서 공유메모리 초기화
-            lst_usr_info = kapi.Get_Login_Info()
+            lst_usr_info = self.kapi.Get_Login_Info()
             self.__shared_mem = SharedMem(lst_usr_info)
 
             self.log = Logger()
             self.log.INFO(self._instance)
+
+    #sharedMem 업데이트하고 바로 DB 업데이트하는 함수 호출, 60초에 한번
+    def update_info(self):
+        t_LastUpdated = datetime.now().timestamp()
+        cls_SM = SharedMem()
+        cls_DB = GetPutDB()
+
+        while True:
+            if t_LastUpdated > const.SM_UPDATE_PERIOD:
+                cls_SM.update_all()
+                cls_DB.update_properties()
+
+                t_LastUpdated = datetime.now().timestamp()
+            
+            time.sleep(1)
+            
+    #매매의사결정 스케쥴러 함수 호출
+    #살지말지 결정 후 다른 스레드(api 호출클래스메소드) 호출
+    def call_price(self):
+        pass
+    
+    #의사결정 스레드의 주문대로 api호출하여 주문한다.
+    def trade_stocks(self):
+        pass
+
