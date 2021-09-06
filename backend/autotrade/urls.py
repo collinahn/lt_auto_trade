6f151766552f91275833aeec30d857ea1f89821f
jiuk96 <jiuk96@naver.com>
2021-09-06 17:20:49 +0900

    [Add] urls 추가

---
+++ backend/autotrade/urls.py
@@ -0,0 +1,10 @@
+#backend/post/urls.py
+from django.urls import path
+from .views import BoardListCreateView, BoardRetrieveUpdateDestroyAPIView
+# ShowCurrentUserInfo
+
+urlpatterns = [
+    path('', BoardListCreateView.as_view(), name=BoardListCreateView.name),
+    path('<int:pk>/', BoardRetrieveUpdateDestroyAPIView.as_view(), name = BoardRetrieveUpdateDestroyAPIView.name),
+    # path('userinfo', ShowCurrentUserInfo.as_view())
+]
